0d2803c2c86d3204d8925450b3a38403e0dd0bcb
gabjuh <jjjjgabesz@gmail.com>
2023-02-20 06:27:03 +0100

    interfaces, newChordObj state created

--- src/components/New/New.tsx
+++ src/components/New/New.tsx
@@ -1,13 +1,44 @@
 import React from "react";
+import ChordTableInput from "../ChordTableInput/ChordTableInput";
+import { useState } from "react";
+import PageTitle from "../PageComponents/PageTitle";
 
 interface New {
 
 }
 
 const New: React.FC<New> = () => {
+
+  interface String {
+    fret: number
+    finger: number
+  }
+
+  interface Chord {
+    name: string
+    base: number
+    strings: (String | null)[]
+  }
+
+  const [newChordObj, setNewChordObj] = useState<Chord>({
+    "name": "?",
+    "base": 0,
+    "strings": [
+      null,
+      null,
+      null,
+      null,
+      null,
+      null
+    ]
+  })
+
   return (
     <>
-      <h2>New</h2>
+      <PageTitle text="New Chord" />
+      <ChordTableInput 
+        newChord={true}
+      />
     </>
   )
 }
