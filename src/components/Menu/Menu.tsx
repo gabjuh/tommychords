f8ead40b18614b27b08a5a7f95f7c69f12cc7d04
gabjuh <jjjjgabesz@gmail.com>
2023-02-20 08:22:54 +0100

    set/isMenuOpen passet to Menu and MenuItem

--- src/components/Menu/Menu.tsx
+++ src/components/Menu/Menu.tsx
@@ -2,34 +2,46 @@ import React from "react";
 import MenuItem from "./MenuItem";
 
 interface Menu {
-
+  isMenuOpen: Boolean
+  setIsMenuOpen: React.Dispatch<React.SetStateAction<Boolean>>
 }
 
-const Menu: React.FC<Menu> = () => {
+const Menu: React.FC<Menu> = ({ isMenuOpen, setIsMenuOpen }) => {
   return (
     <>
       {/* -rotate-90 hidden opacity-0 */}
-      <div id="menu" className="absolute pb-2 pt-8 -mt-7 ml-2 border dark:border-[#eee] border-[#222] dark:bg-[#222]  w-[7rem] rounded-b-3xl z-40 transform-gpu -rotate-90 hidden opacity-0  origin-top-left transition ease-out duration-150">
+      <div id="menu" className={`absolute pb-2 pt-8 -mt-7 ml-2 border dark:border-[#eee] border-[#222] dark:bg-[#222] w-[7rem] rounded-b-3xl z-40 transform-gpu transition ease-out duration-150 ${!isMenuOpen && 'hidden'}`}>
+        {/* hidden opacity-0 */}
         {/* <p className="text-[#777]">Menü</p> */}
-        <MenuItem 
-          name="Overview" 
+        <MenuItem
+          name="Overview"
           route="/overview"
+          isMenuOpen={isMenuOpen}
+          setIsMenuOpen={setIsMenuOpen}
         />
-        <MenuItem 
-          name="New" 
+        <MenuItem
+          name="New"
           route="/new"
+          isMenuOpen={isMenuOpen}
+          setIsMenuOpen={setIsMenuOpen}
         />
-        <MenuItem 
-          name="Let's play!" 
+        <MenuItem
+          name="Let's play!"
           route="/play"
+          isMenuOpen={isMenuOpen}
+          setIsMenuOpen={setIsMenuOpen}
         />
-        <MenuItem 
-          name="Help" 
+        <MenuItem
+          name="Help"
           route="/help"
+          isMenuOpen={isMenuOpen}
+          setIsMenuOpen={setIsMenuOpen}
         />
-        <MenuItem 
-          name="Credits" 
+        <MenuItem
+          name="Credits"
           route="/credits"
+          isMenuOpen={isMenuOpen}
+          setIsMenuOpen={setIsMenuOpen}
         />
       </div>
     </>
--- src/components/Menu/MenuItem.tsx
+++ src/components/Menu/MenuItem.tsx
@@ -4,17 +4,20 @@ import { Link } from 'react-router-dom'
 interface MenuItem {
   name: string
   route: string
+  isMenuOpen: Boolean
+  setIsMenuOpen: React.Dispatch<React.SetStateAction<Boolean>>
 }
 
-const MenuItem: React.FC<MenuItem> = ({ name, route }) => {
+const MenuItem: React.FC<MenuItem> = ({ name, route, setIsMenuOpen }) => {
   return (
     <>
-          <Link 
-            to={route}
-            className="block dark:text-[#ccc] text-[#222] dark:hover:text-[#fff] hover:text-[#222] hover:text-[1.2rem] cursor-pointer my-1 transition-all ease-in-out duration-150" 
-          >            
-            {name}
-          </Link>
+      <Link
+        to={route}
+        className="block dark:text-[#ccc] text-[#222] dark:hover:text-[#fff] hover:text-[#222] hover:text-[1.2rem] cursor-pointer my-1 transition-all ease-in-out duration-150"
+        onClick={() => setIsMenuOpen(false)}
+      >
+        {name}
+      </Link>
     </>
   )
 }
