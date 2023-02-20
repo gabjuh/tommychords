c30f13d7823c76d2497b31ad9c7a5bffd0800b5f
gabjuh <jjjjgabesz@gmail.com>
2023-02-20 06:22:48 +0100

    finger tip theme colors set, margin of numbers changed

--- src/components/ChordTableInput/Finger.tsx
+++ src/components/ChordTableInput/Finger.tsx
@@ -13,12 +13,12 @@ const Finger: React.FC<Finger> = ({ griff, line }) => {
 
   return (
     <>
-      <div className="absolute opacity-0 hover:opacity-100 w-8 h-8 rounded-3xl border bg-[#222] left-[-1rem] bottom-[1rem] cursor-pointer transition ease-in duration-100"
+      <div className="absolute dark:text-[#ddd] opacity-0 hover:opacity-100 w-8 h-8 rounded-3xl border border-[#333] dark:border-[#ddd] dark:bg-[#222] bg-[#fff] left-[-1rem] bottom-[1rem] cursor-pointer transition ease-in duration-100"
       style={{
         opacity: griff !== null && griff.fret === line ? 1 : 0
       }}
       >
-        <p className="mt-[.2rem]">
+        <p className="mt-[.22rem] mr-[.03rem]">
           {griff !== null ? griff.finger : ''}
         </p >
       </div>
