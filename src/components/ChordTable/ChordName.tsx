326128842cd055dc25117d6e7171184749258d54
gabjuh <jjjjgabesz@gmail.com>
2023-02-20 10:19:43 +0100

    chordNav removed from chordName component

--- src/components/ChordTable/ChordName.tsx
+++ src/components/ChordTable/ChordName.tsx
@@ -1,26 +1,14 @@
 import React from "react";
-import ChordNavigation from "./ChordNavigation";
 
 interface ChordName {
   name: string
-  selectedId: [
-    selectedId: number,
-    setSelectedId: Function,
-    objLength: number
-  ]
 }
 
-const ChordName: React.FC<ChordName> = ({ name, selectedId }) => {
-
-  const [id, setId, objLen] = selectedId
-
+const ChordName: React.FC<ChordName> = ({ name }) => {
 
   return (
     <>
       <h1 className="dark:text-[#ddd] text-4xl mt-5">{name}</h1>
-      <ChordNavigation
-        selectedId={selectedId}
-      />
     </>
   )
 }
