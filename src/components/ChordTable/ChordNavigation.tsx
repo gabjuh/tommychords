fc12cfa4ebbcc2adb6c75668f2cd66bce2b278f0
gabjuh <jjjjgabesz@gmail.com>
2023-02-20 09:51:00 +0100

    cleanup, setSelectedId consts' name changed

--- src/components/ChordTable/ChordNavigation.tsx
+++ src/components/ChordTable/ChordNavigation.tsx
@@ -1,41 +1,43 @@
 import React from "react";
 
 interface ChordNavigation {
-  setSelectedId: [setSelectedId: number, setSelectedId: Function, objLength: number]
+  setSelectedId: [selectedId: number,
+    setSelectedId: Function,
+    objLength: number]
 }
 
 const ChordNavigation: React.FC<ChordNavigation> = ({ setSelectedId }) => {
 
-  const [val, fn, len] = setSelectedId
+  const [id, setId, objLen] = setSelectedId
 
   return (
     <>
       <div className="dark:text-[#ddd] relative h-6 w-56 mx-auto mb-10 ">
         {/* Backward */}
-        <span 
-          className="absolute left-[3rem] bottom-[-.5rem] px-5 cursor-pointer text-5xl select-none" 
+        <span
+          className="absolute left-[3rem] bottom-[-.5rem] px-5 cursor-pointer text-5xl select-none"
           style={{
-            display: val > 0 ? 'inline-block' : 'none'
+            display: id > 0 ? 'inline-block' : 'none'
           }}
-          onClick={() => fn(val - 1)}
+          onClick={() => setId(id - 1)}
         >
           &lsaquo;
         </span>
 
         <span>
-          {val + 1} / {len}
+          {id + 1} / {objLen}
         </span>
-      
+
         {/* Forward */}
-        <span 
-          className="absolute right-[3rem] bottom-[-.5rem] px-5 cursor-pointer text-5xl select-none" 
+        <span
+          className="absolute right-[3rem] bottom-[-.5rem] px-5 cursor-pointer text-5xl select-none"
           style={{
-            display: val < len - 1 ? 'inline-block' : 'none'
+            display: id < objLen - 1 ? 'inline-block' : 'none'
           }}
-          onClick={() => fn(val + 1)}
+          onClick={() => setId(id + 1)}
         >
           &rsaquo;
-        </span> 
+        </span>
       </div>
     </>
   )
