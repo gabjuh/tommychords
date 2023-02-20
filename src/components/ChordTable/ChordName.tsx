10a50542ea10c3868e0819e8fb289825c4d85ab5
gabjuh <jjjjgabesz@gmail.com>
2023-02-20 09:02:25 +0100

    cleaned up

--- src/components/ChordTable/ChordName.tsx
+++ src/components/ChordTable/ChordName.tsx
@@ -3,14 +3,17 @@ import ChordNavigation from "./ChordNavigation";
 
 interface ChordName {
   name: string
-  setSelectedId: [setSelectedId: number, setSelectedId: Function, objLength: number]
+  setSelectedId: [
+    setSelectedId: number,
+    setSelectedId: Function, objLength: number
+  ]
 }
 
 const ChordName: React.FC<ChordName> = ({ name, setSelectedId }) => {
   return (
-    <> 
+    <>
       <h1 className="dark:text-[#ddd] text-4xl mt-5">{name}</h1>
-      <ChordNavigation 
+      <ChordNavigation
         setSelectedId={setSelectedId}
       />
     </>
